7e9629218b81a01d8bbbc685116faed2f64c190a
Marish Lakshmanan <sakthi9481@gmail.com>
2025-03-25 15:42:13 -0400

    Initial commit with TaialwindCSS Intialized with react.js for frontend

---
+++ dbms/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import DndBoard from './Components/DndBoard'
+
+function App() {
+
+
+  return (
+    <>
+    <div className='text-amber-300'>
+    <DndBoard />
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ dbms/src/Components/Droppable.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {useDroppable} from '@dnd-kit/core';
+
+export default function Droppable(props) {
+  const {isOver, setNodeRef} = useDroppable({
+    id: props.id,
+  });
+  const style = {
+    color: isOver ? 'green' : undefined,
+  };
+  
+  
+  return (
+    <div ref={setNodeRef} style={style} className='w-1/2 h-[50vh] bg-amber-800'>
+      {props.children}
+    </div>
+  );
+}
