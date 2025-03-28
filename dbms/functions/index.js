a3b27097398596ae80f0ddd63a4e3bf9720338d9
Michael McInerney <michael.mcinerney@thalrix.com>
2025-03-27 22:26:42 -0400

    Mild restructuring, removing cache directories that made their way in

--- dbms/functions/index.js
+++ dbms/functions/index.js
@@ -7,13 +7,67 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+// Import necessary functions and add requirements as needed
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+// The Cloud Functions for Firebase SDK to create Cloud Functions and triggers.
+import {logger} from "firebase-functions";
+import {onRequest} from "firebase-functions/v2/https";
+
+// Your web app's Firebase configuration
+import {} from 'dotenv/config'
+const FIREBASE_APIKEY = process.env.API_KEY;
+
+const firebaseConfig = {
+  apiKey: FIREBASE_APIKEY,
+  authDomain: "dbms-wpi-team8.firebaseapp.com",
+  projectId: "dbms-wpi-team8",
+  storageBucket: "dbms-wpi-team8.firebasestorage.app",
+  messagingSenderId: "718872483196",
+  appId: "1:718872483196:web:a87cbbdaf48fdd1a7800f9",
+  measurementId: "G-E5ECVGZN7G"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.helloWorld = onRequest((request, response) => {
+  logger.info("Hello logs!", {structuredData: true});
+  response.send("Hello from Firebase!");
+});
+
+signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+        return userCredential.user.getIdToken(); // <- Send this token to backend
+    })
+    .then((idToken) => {
+        // Send idToken to your backend via request header
+    });
+
+window.login = async function () {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  try {
+    const userCredential = await signInWithEmailAndPassword(auth, email, password);
+    const idToken = await userCredential.user.getIdToken();
+    
+    const response = await fetch("/protected", {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${idToken}`
+      }
+    });
+
+    const result = await response.text();
+    alert(result);
+  } catch (err) {
+    console.error(err);
+    alert("Login failed");
+  }
+};
--- dbms/src/App.jsx
+++ dbms/src/App.jsx
@@ -2,41 +2,6 @@
 import './App.css'
 import DndBoard from './Components/DndBoard'
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-require('dotenv').config();
-
-const FIREBASE_APIKEY = process.env.API_KEY;
-
-const firebaseConfig = {
-  apiKey: FIREBASE_APIKEY,
-  authDomain: "dbms-wpi-team8.firebaseapp.com",
-  projectId: "dbms-wpi-team8",
-  storageBucket: "dbms-wpi-team8.firebasestorage.app",
-  messagingSenderId: "718872483196",
-  appId: "1:718872483196:web:a87cbbdaf48fdd1a7800f9",
-  measurementId: "G-E5ECVGZN7G"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
-signInWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    return userCredential.user.getIdToken(); // <- Send this token to backend
-  })
-  .then((idToken) => {
-    // Send idToken to your backend via request header
-  });
-
 function App() {
 
 
